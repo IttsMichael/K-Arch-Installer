7ba5d47d12aababc57ab9d9e23f3219d2167cc1d
michael <davletkildeevmichael@gmail.com>
2026-01-23 18:19:08 +0100

    timezone combo

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ datadisk = subprocess.check_output(
     text=True
 )
 
+timezones = subprocess.check_output(
+    ["timedatectl", "list-timezones"],
+    text=True).splitlines()
+
 for dev in json.loads(datadisk)["blockdevices"]:
     if dev ["type"] == "disk":
         model = dev["model"] or "Unknown device"
@@ -29,6 +33,7 @@ for dev in json.loads(datadisk)["blockdevices"]:
         pathdisk = f"/dev/{name}"
 
         disks.append((displaydisk, pathdisk))
+
 
 def toggle_swap(enabled: bool):
     window.spinSwap.setEnabled(enabled)
@@ -56,12 +61,19 @@ def next_clicked():
     global page
     page += 1
     if page == 1:
+        print("page1")
         page1()
-    
+    elif page == 2:
+        print("page2")
+        page2()
 
 def page1():
     window.stackedWidget.setCurrentIndex(1)
-    window.comboBox.addItems(d[0] for d in disks)
+    window.comboZone.addItems(timezones)
+
+def page2():
+    window.stackedWidget.setCurrentIndex(2)
+    window.comboDisk.addItems(d[0] for d in disks)
     window.savedisks.clicked.connect(saved)
 
 
@@ -78,6 +90,8 @@ window.swapCheck.toggled.connect(toggle_swap)
 toggle_swap(window.swapCheck.isChecked())
 next_btn = window.findChild(QPushButton, "nextButton")
 next_btn.clicked.connect(next_clicked)
+next_btn2 = window.findChild(QPushButton, "nextButton_2")
+next_btn2.clicked.connect(next_clicked)
 
 window.show()
 window.stackedWidget.setCurrentIndex(0)
